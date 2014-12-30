817a06cbf52c8d521fce9cc9d4641b3c9ec5c675
Eric Anderson <ericanderson1011@gmail.com>
2014-12-30 00:27:03 -0600

    Initial Commit

---
+++ ALGAlgorithmsEdu/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ALGAlgorithmsEdu
+//
+//  Created by Eric Anderson on 12/30/14.
+//  Copyright (c) 2014 Eric Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
